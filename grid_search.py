5521720f285cbdff49b75bdc818ed54f6d0b12f4
in-droid <ivannikolovmail@gmail.com>
2023-12-29 13:47:44 +0100

    Update code for second part

--- grid_search.py
+++ grid_search.py
@@ -1,29 +1,38 @@
 import subprocess
+from tqdm import tqdm
 # from ultralytics import YOLO
 import os
 import itertools
 import datetime
 import pickle
-import subprocess
 
-def train_model(lr, dropout, wd, optimizer):
+
+def train_model(lr, dropout, wd, optimizer, augment_dict=None):
+    if augment_dict is None:
+        augment_dict = {}
+        augment = False
+    else:
+        augment = True
+
     command = [
         "python3",  # or specify the path to your Python executable
         "train_script.py",  # replace with the actual script name
         "-lr", str(lr), 
         '-dp', str(dropout), 
         '-wd', str(wd), 
-        '-o', optimizer]
+        '-o', optimizer,
+        '--augment' if augment else '',
+        '--hsv_h', str(augment_dict.get('hsv_h', 0)),
+        '--hsv_s', str(augment_dict.get('hsv_s', 0)),
+        '--hsv_v', str(augment_dict.get('hsv_v', 0)),
+        '--degrees', str(augment_dict.get('degrees', 0)),
+        '--mixup', str(augment_dict.get('mixup', 0))
+        ]
 
     
     result = subprocess.run(command, capture_output=True, check=True)
     stdout = result.stdout.decode()  # Decode the stdout bytes to a string
     metric = stdout.split('\n')[-2].split(' ')[-1]
-    # Process the stdout as needed
-    # print(stdout)
-    # print('*'*50)
-    # print(stdout.split('\n')[-2])
-    # print('*'*50)
     return float(metric)
 
 if __name__ == '__main__':
@@ -35,21 +44,63 @@ if __name__ == '__main__':
     dname = os.path.dirname(os.path.abspath(__file__))
     os.chdir(dname)
 
-    learning_rates = [0.0001, 0.001, 0.01]
-    dropouts = [0.01, 0.1, 0.5]
-    weight_decays = [0.0005, 0.005, 0.05]
-    optimizers = ['SGD', 'Adam']
+    # learning_rates = [0.0001, 0.001, 0.01]
+    learning_rate = 0.00
+    dropout = 0.01
+    wd = 0.005
+    optimizer = 'Adam'
+    # best metrics from the first step
 
+    hsv_h_params = [0.015, 0.1, 0.2]
+    hsv_s_params = [0.1, 0.7]
+    hsv_v_params = [0.1, 0.4]
+    degrees_params = [0, 45]
+    mixup_params = [0, 0.2, 0.5]
+    
 
     param_combinations = list(itertools.product(
-        learning_rates, dropouts, weight_decays, optimizers))
+        hsv_h_params, 
+        hsv_s_params, 
+        hsv_v_params, 
+        degrees_params,
+        mixup_params
+        ))
 
-    for lr, dropout, wd, optimizer in param_combinations:
-        mAp = train_model(lr, dropout, wd, optimizer)
+
+    print('Grid search started.')
+    print('*'*50)
+    print(f'Number of combinations: {len(param_combinations)}')
+    print("| ".join(['hsv_h', 'hsv_s', 'hsv_v', 'degrees', 'mixup', " mAP"]))
+    for hsv_h, hsv_s, hsv_v, degrees, mixup in tqdm(param_combinations):
+        mAp = train_model(learning_rate, dropout, wd, optimizer, augment_dict={
+            'hsv_h': hsv_h,
+            'hsv_s': hsv_s,
+            'hsv_v': hsv_v,
+            'degrees': degrees,
+            'mixup': mixup
+        }
+        )
+        print(f'{hsv_h} | {hsv_s} | {hsv_v} | {degrees} | {mixup} || {mAp: .3f}')
         if mAp > best_map:
             best_map = mAp
-            best_params = (lr, dropout, wd, optimizer)
-    print("Training completed.")
-    print("*"*50)
-    print(f"Best mAP: {best_map}, Best params: {best_params}")
+            best_params = (hsv_h, hsv_s, hsv_v, degrees, mixup)
+    print('Training completed.')
+    print('*'*50)
+    best_params_with_names = zip(
+        ['hsv_h', 'hsv_s', 'hsv_v', 'degrees', 'mixup'],
+        best_params
+    )
+
+    params_string = ', '.join([f'{name}: {value}' for name, value in best_params_with_names])
+
+    print(f'Best mAP: {best_map}, Best params: {params_string}')
+
+    # for lr, dropout, wd, optimizer in tqdm(param_combinations):
+    #     mAp = train_model(lr, dropout, wd, optimizer)
+    #     if mAp > best_map:
+    #         best_map = mAp
+    #         best_params = (lr, dropout, wd, optimizer)
+    # print("Training completed.")
+    # print("*"*50)
+    # print(f"Best mAP: {best_map}, Best params: {best_params}")
 
--- train_augmentations.py
+++ train_augmentations.py
@@ -4,20 +4,12 @@ from ultralytics import YOLO
 import os, itertools, datetime
 import pickle
 
-OUTPUT_PATH = 'runs/detect/grid_search'
-
 def parse_args():
     parser = argparse.ArgumentParser(description='Training Script')
     parser.add_argument('-lr', '--learning_rate', type=float, default=0.001, help='Learning rate')
     parser.add_argument( '-dp' ,'--dropout', type=float, default=0.5, help='Dropout rate')
     parser.add_argument('-wd', '--weight_decay', type=float, default=0.0001, help='Weight decay')
     parser.add_argument('-o', '--optimizer', type=str, default='adam', help='Optimizer')
-    parser.add_argument('-a', '--augment', action='store_true', help='Augment data')
-    parser.add_argument('--hsv_h', type=float, default=0, help='HSV-Hue augmentation')
-    parser.add_argument('--hsv_s', type=float, default=0, help='HSV-Saturation augmentation')
-    parser.add_argument('--hsv_v', type=float, default=0, help='HSV-Value augmentation')
-    parser.add_argument('--degrees', type=float, default=0, help='Rotation augmentation')
-    parser.add_argument('--mixup', type=float, default=0, help='Mixup augmentation')
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -30,14 +22,14 @@ if __name__ == "__main__":
     model = YOLO("yolov8n.pt")
     model.train(
         data="ears.yaml", 
-        epochs=20, 
+        epochs=1, 
         optimizer=optimizer, 
         pretrained=True, 
         patience=3,
         plots=True,
         batch=32,
         val=True,
-        augment=args.augment,
+        augment=False,
         dropout=dropout,
         lr0=learning_rate,
         lrf=0.2,
@@ -47,20 +39,10 @@ if __name__ == "__main__":
         warmup_momentum=0.5,
         warmup_bias_lr=0.1,
         box=7.5,
-        cls=0.5,
-        hsv_h=args.hsv_h,
-        hsv_s=args.hsv_s,
-        hsv_v=args.hsv_v,
-        degrees=args.degrees,
-        mixup=args.mixup
+        cls=0.5
     )
     metrics = model.val() # It'll automatically evaluate the data you trained.
-    if args.augment:
-        output_path = os.path.join(OUTPUT_PATH, 'augment', f'hsv_h_{args.hsv_h}_hsv_s_{args.hsv_s}_hsv_v_{args.hsv_v}_degrees_{args.degrees}_mixup_{args.mixup}')
-    else:
-        output_path = os.path.join(OUTPUT_PATH, f'train_{learning_rate}_{dropout}_{weight_decay}_{optimizer}')
-    Path(output_path).mkdir(parents=True, exist_ok=True)
-    with open(os.path.join(output_path, 'metrics.pkl'), 'wb') as f:
-        pickle.dump(metrics.results_dict, f)  
+    Path(f'runs/detect/grid_search/train_{learning_rate}_{dropout}_{weight_decay}_{optimizer}').mkdir(parents=True, exist_ok=True)
+    with open(f'runs/detect/grid_search/train_{learning_rate}_{dropout}_{weight_decay}_{optimizer}/metrics.pkl', 'wb') as f:
+        pickle.dump(metrics.results_dict, f)
     print("MAP:", metrics.results_dict['metrics/mAP50-95(B)'])
-    
